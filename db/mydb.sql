b6b807ea92807a844b95fafd970b1509d72c07ee
Jorge Lopez <Jorgelopez5252@gmail.com>
2022-11-12 20:18:36 -0700

    commit all

---
+++ db/mydb.sql
@@ -0,0 +1,10 @@
+-- DROP DATABASE IF EXISTS mydb;
+-- CREATE DATABASE mydb;
+
+-- USE mydb;
+
+-- CREATE TABLE user (
+--     id int PRIMARY KEY,
+--     name VARCHAR(30) NOT NULL,
+--     password VARCHAR(30) NOT NULL
+-- )
